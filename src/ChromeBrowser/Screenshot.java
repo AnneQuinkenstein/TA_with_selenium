69bcca9c0130e72f254b968cb58ea5592c69b192
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-02-05 18:59:21 +0100

    fixed taking screenshot

--- src/ChromeBrowser/Screenshot.java
+++ src/ChromeBrowser/Screenshot.java
@@ -4,25 +4,28 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FileUtils;
 
 //package FileUtils is not activated ->
 
 import java.io.File;
+import java.io.IOException;
 
 public class Screenshot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String url = "https://google.de";
         System.setProperty("webdriver.chrome.driver", "//home//lisa//IdeaProjects//chromedriver");
         WebDriver driver = new ChromeDriver();
 
-//        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        driver.get(url);
+
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         //cast your driver-object to take screenshot
         //take Screenshot
         //get the output as a file
         //-> take Screenshot, whn google is opend and store it
 
-       // FileUtils.copyFile(src, new File (C://home/lisa/IdeaProjects/Udemy/Java_puzzels/SeleniumIntroduction/Screenshot.png)
-        //writing file to lokal machine
+         FileUtils.copyFile(src, new File ("screenshot.png"));
+        //writing file to local machine
     }
 }
